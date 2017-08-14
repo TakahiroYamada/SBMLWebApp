b3a3c2fe66cf8c1bd22219da7873f0300ccdea73
Takahiro Yamada <yamada@fun.bio.keio.ac.jp>
2017-08-14 17:47:08 +0900

    Steady state analysis : error message is added

--- WebContent/steadyEvent.js
+++ WebContent/steadyEvent.js
@@ -1,10 +1,51 @@
 $("#stedButton").on("click" , function(){
-	$(this).LoadingOverlay("show");
-	getSteadyResult( $(this));
-})
-$(".sted-param").on("keypress" , function( e ){
-	if( e.which == 13 ){
-		$("#stedButton").LoadingOverlay("show");
-		getSteadyResult( $("#stedButton"));
+	if( !errorCheck()){
+		$(this).LoadingOverlay("show");
+		getSteadyResult( $(this));
 	}
 })
+
+$(".sted-param").on("keypress" , function( e ){
+	if( e.which == 13 ){
+		if( !errorCheck()){
+			$("#stedButton").LoadingOverlay("show");
+			getSteadyResult( $("#stedButton"));
+		}
+	}
+})
+function errorCheck(){
+	var info = checkNegativeValueinInput( $("input").each(function(index){
+	}));
+	// If file is not selected the error is visualized
+	if( $("#stedFile").val().length == 0){
+		errorSetting("SBML model is not selected" , "Selecting SBML file in input form")
+		$("#warningModal").modal("show");
+		$("#modalButton").off("click");
+		$("#modalButton").on("click" , function(){
+			$("#warningModal").modal("hide");
+			var el = $("#stedFile");
+			newOne = el.clone( true);
+			el.before( newOne );
+			el.remove();
+			newOne.addClass("animated flash");
+		})
+		return true;
+	}
+	else if( info.isNegative ){
+		errorSetting("Some value is negative" , "Set the value larger than 0")
+		$("#warningModal").modal("show");
+		$("#modalButton").off("click");
+		$("#modalButton").on("click" , function(){
+			$("#warningModal").modal("hide");
+			for( var i = 0 ; i < info.contents.length ; i ++){
+				var el = info.contents[ i ];
+				newOne = el.clone( true );
+				el.before( newOne);
+				el.remove();
+				newOne.addClass("animated flash");
+			}
+		});
+		return true
+	}
+	return false;
+}
