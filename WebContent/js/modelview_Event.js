6b2b4cef3ab5db39a187d37852d2bedb243172c9
Takahiro Yamada <yamada@fun.bio.keio.ac.jp>
2017-09-29 12:57:54 +0900

    Modal of graph view is added

---
+++ WebContent/js/modelview_Event.js
@@ -0,0 +1,6 @@
+$("#nav-graphview").on("click" , function(){
+	$("#modelgraph-Modal").modal("show");
+})
+$("#graphmodalButton").on("click" , function(){
+	$("#modelgraph-Modal").modal("hide");
+});
